8eed5f381040468e3b9da6a2caf88153c06a75b9
arc363 <arc3363@yahoo.co.uk>
2023-10-30 14:48:09 +0000

    inserted database portion

--- main.js
+++ main.js
@@ -63,13 +63,46 @@ function readInData(id) {
     function (err, data) {
       if (err) throw err;
       obj = JSON.parse(data);
-      //   console.log(obj);
+      // console.log(obj);
       return obj;
     }
   );
 }
 
 // // database portion
+import { Pool } from "pg"; // pg is the postgres database client package
+
+// This configures our connection pool
+export const pool = new Pool({
+  user: "postgres",
+  host: "localhost",
+  database: "phylogenetic-data",
+  password: "postgres",
+  port: 5432,
+});
+
+// A basic error message if the connection pool dies
+pool.on("error", (err) => {
+  console.error("Unexpected error on idle client", err);
+  process.exit(-1);
+});
+// A function to take the id and data and insert it into the correct row of the database
+export async function uploadSubmission(id, jsonData) {
+  const client = await pool.connect();
+  try {
+    await client.query(
+      "UPDATE public.submissions SET status_id=5, processed_data=$2 WHERE id=$1;",
+      [id, jsonData]
+    );
+    return true;
+  } catch (error) {
+    console.log(error);
+    return false;
+  } finally {
+    client.release();
+  }
+}
+
 // open database connection
 // Run command to insert the data and set the status
 // Close connection
