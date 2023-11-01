4ddbe39617c3f815588b488b76308e264e2c51b4
arc363 <arc3363@yahoo.co.uk>
2023-11-01 12:16:07 +0000

    main.js now working Co-authored-by: Sean Milne <Thrudd@users.noreply.github.com>

--- main.js
+++ main.js
@@ -1,7 +1,22 @@
 const args = require("args-parser")(process.argv);
 const path = require("path");
 const fs = require(`fs`);
+const pg = require("pg"); // pg is the postgres database client package
 
+// This configures our connection pool
+const pool = new pg.Pool({
+  user: "postgres",
+  host: "localhost",
+  database: "chagatyper",
+  password: "postgres",
+  port: 5432,
+});
+
+// A basic error message if the connection pool dies
+pool.on("error", (err) => {
+  console.error("Unexpected error on idle client", err);
+  process.exit(-1);
+});
 // Get id from run time parameters
 if (args.id) {
   console.log(`Request ID supplied`);
@@ -30,7 +45,7 @@ function uploadFolder(id) {
   if (!fs.existsSync(`./${requestID}`)) {
     fs.mkdir(`./${requestID}`, (err) => {
       if (err) {
-        console.log(err);
+        // console.log(err);
       }
       console.log(`Folder created`);
     });
@@ -45,7 +60,7 @@ function checkJson(id) {
   if (!fs.existsSync(`./${requestID}.json`)) {
     fs.readdir(`./${requestID}.json`, (err) => {
       if (err) {
-        console.log(err);
+        // console.log(err);
       }
       console.log(`Failed to locate JSON file`);
     });
@@ -53,45 +68,30 @@ function checkJson(id) {
     console.log(`JSON file located`);
   }
 }
-/*
-// Read the json into a variable
-readInData(requestID);
-function readInData(id) {
-  return fs.readFile(
-    path.resolve(__dirname, `${id}.json`),
-    "utf8",
-    function (err, data) {
-      if (err) throw err;
-      obj = JSON.parse(data);
-      // console.log(obj);
-      return obj;
-    }
-  );
+
+// Read the json into a variable IF the corresponding JSON file was located
+if (fs.existsSync(`./${requestID}.json`)) {
+  let obj = readInData(requestID);
+  function readInData(id) {
+    return fs.readFile(
+      path.resolve(__dirname, `${id}.json`),
+      "utf8",
+      function (err, data) {
+        if (err) throw err;
+        obj = JSON.parse(data);
+        uploadSubmission(requestID, obj);
+      }
+    );
+  }
+  console.log(`JSON data read into variable`);
 }
 
-// // database portion
-const pool = require("pg"); // pg is the postgres database client package
-
-// This configures our connection pool
-export const pool = new Pool({
-  user: "postgres",
-  host: "localhost",
-  database: "phylogenetic-data",
-  password: "postgres",
-  port: 5432,
-});
-
-// A basic error message if the connection pool dies
-pool.on("error", (err) => {
-  console.error("Unexpected error on idle client", err);
-  process.exit(-1);
-});
-// A function to take the id and data and insert it into the correct row of the database
-export async function uploadSubmission(id, jsonData) {
+// A function to take the id and data and insert them into the correct row of the database
+async function uploadSubmission(id, jsonData) {
   const client = await pool.connect();
   try {
     await client.query(
-      "UPDATE public.submissions SET status_id=5, processed_data=$2 WHERE id=$1;",
+      "UPDATE public.submissions SET status_id=5, clinical_classification='chocolate cake', processed_data=$2 WHERE id=$1;",
       [id, jsonData]
     );
     return true;
@@ -102,7 +102,7 @@ export async function uploadSubmission(id, jsonData) {
     client.release();
   }
 }
-*/
+
 // open database connection
 // Run command to insert the data and set the status
 // Close connection
